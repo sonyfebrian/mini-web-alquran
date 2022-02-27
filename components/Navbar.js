fdb709f481be4ae33c84a3b2c1ba0bea54919fdf
sony <sonyfebrian362@gmail.com>
2022-02-27 18:05:37 +0700

    add-banner

---
+++ components/Navbar.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+
+export default function Header() {
+  const menuItems = [
+    { title: 'Home', url: 'https://themeptation.net' },
+    { title: 'Products', url: 'https://themeptation.net' },
+    { title: 'Contact', url: 'https://themeptation.net' },
+  ];
+
+  const [animateHeader, setAnimateHeader] = useState(false);
+
+  useEffect(() => {
+    const listener = () => {
+      if (window.scrollY > 140) {
+        setAnimateHeader(true);
+      } else setAnimateHeader(false);
+    };
+    window.addEventListener('scroll', listener); return () => {
+      window.removeEventListener('scroll', listener);
+    };
+  }, []);
+  return (
+    <header
+      className={`w-full backdrop-filter backdrop-blur-lg bg-white/50 fixed z-10 trasition ease-in-out duration-500 ${
+        animateHeader && 'shadow-xl'
+      }`}
+    >
+      <div className="max-w-7xl mx-auto ">
+        <div
+          className="flex max-w-screen-xl py-10 mx-auto items-center justify-between px-8 trasition ease-in-out duration-500"
+        >
+          <a
+            href="https://themeptation.net"
+            className="text-xl font-bold tracking-tighter text-indigo-400 pr-8"
+          >
+            Themeptation
+          </a>
+          <nav>
+            <ul className="flex items-center justify-start">
+              {menuItems?.map((item) => (
+                <li key={item?.title}>
+                  <Link href={item?.url}>
+                    <a className="px-2 lg:px-6 py-6 text-md border-b-2 border-transparent hover:border-indigo-400 leading-[22px] md:px-3 text-gray-400 hover:text-indigo-500">
+                      {item?.title}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Surah from '../components/surah';
 import Header from '../components/header';
 import ApiService from '../services/api';
+import NavBar from '../components/Navbar';
 
 export async function getStaticProps() {
   const surahs = await ApiService.getAllSurah();
@@ -20,6 +21,39 @@ export async function getStaticProps() {
 export default function Home({ recommendedSurahs }) {
   return (
     <>
+      <NavBar />
+      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div className="flex flex-col items-center justify-center py-2">
+            <div className="absolute inset-0 bg-gradient-to-r from-indigo-200 to-purple-600 shadow-lg transform -rotate-45 rotate-6 rounded-3xl" />
+            <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+              <h1 className="text-6xl font-bold">
+                Stylish Glassmorphic Header Animation Made with Tailwind CSS &
+                Next.js
+              </h1>
+              <h2 className="text-3xl font-bold">
+                Fork and modify this sandbox
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div className="flex flex-col items-center justify-center py-2">
+            <div className="absolute inset-0 bg-gradient-to-r from-indigo-200 to-purple-600 shadow-lg transform -rotate-45 rotate-6 rounded-3xl" />
+            <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+              <h1 className="text-6xl font-bold">
+                Stylish Glassmorphic Header Animation Made with Tailwind CSS &
+                Next.js
+              </h1>
+              <h2 className="text-3xl font-bold">
+                Fork and modify this sandbox
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
       <Header />
       <main className="container">
         <div className="bg-teal-600 rounded-lg shadow py-4 px-4 my-4">
