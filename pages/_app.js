de93a81c15e7088e196a8baffa8f50abc7bb0bc3
sony <sonyfebrian362@gmail.com>
2022-03-10 22:29:50 +0700

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,19 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
 import NProgress from 'nprogress';
-// eslint-disable-next-line import/no-unresolved
-import { route } from 'next/dist/server/router';
-import { useRouter, Router } from 'next/router';
-import { useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import * as ga from '../services/analytics';
+import Router from 'next/router';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
-
-const router = useRouter();
-
-useEffect(() => {
-  const handleRouteChange = (url) => {
-    ga.pageview(url);
-  };
-  router.events.on('routeChangeComplete', handleRouteChange);
-  return () => {
-    router.events.off('routeChangeComplete', handleRouteChange);
-  };
-}, [router.events]);
 
 // eslint-disable-next-line react/prop-types
 export default function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return (
     <>
-      <Navbar />
 
-      <Component {...pageProps} key={route} />
+      <Component {...pageProps} />
     </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -20,23 +20,31 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icons/icon-144x144.png" />
           <script
-            async
             src="https://www.googletagmanager.com/gtag/js?id=G-K0S3G7R59J"
+            strategy="afterInteractive"
           />
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', 'G-K0S3G7R59J', {
-                                page_path: window.location.pathname,
-                                });
-                            `,
-            }}
-          />
+          <script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-K0S3G7R59J');
+        `}
+          </script>
+          <script id="google-analytics" strategy="afterInteractive">
+            {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-XXXXX-Y', 'auto');
+          ga('send', 'pageview');
+        `}
+          </script>
         </Head>
+
         <body>
           <Main />
           <NextScript />
