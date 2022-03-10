b2e05e6789502d7fd3a8f7864ca08059c1c9cfcb
sony <sonyfebrian362@gmail.com>
2022-03-10 22:02:43 +0700

    Revert "add-ga"
    This reverts commit 6fa4920030ef12a36ee185ef3a148e2f73155ab4.

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
 import NProgress from 'nprogress';
-// eslint-disable-next-line import/no-unresolved
-import { route } from 'next/dist/server/router';
-import { useRouter, Router } from 'next/router';
-import { useEffect } from 'react';
+import Router from 'next/router';
 import Navbar from '../components/Navbar';
-import * as ga from '../services/analytics';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
-
-const router = useRouter();
-
-useEffect(() => {
-  const handleRouteChange = (url) => {
-    ga.pageview(url);
-  };
-  router.events.on('routeChangeComplete', handleRouteChange);
-  return () => {
-    router.events.off('routeChangeComplete', handleRouteChange);
-  };
-}, [router.events]);
 
 // eslint-disable-next-line react/prop-types
 export default function MyApp({ Component, pageProps }) {
@@ -31,7 +15,7 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Navbar />
 
-      <Component {...pageProps} key={route} />
+      <Component {...pageProps} />
     </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -19,23 +19,6 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=family=Source+Sans+Pro:wght@400;600;700&display=swap" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icons/icon-144x144.png" />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-K0S3G7R59J"
-          />
-          <script
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', 'G-K0S3G7R59J', {
-                                page_path: window.location.pathname,
-                                });
-                            `,
-            }}
-          />
         </Head>
         <body>
           <Main />
